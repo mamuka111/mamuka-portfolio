1aa93a276b51a8d00c0bcfcfa86ad22002cbcad8
mamuka111 <123620275+mamuka111@users.noreply.github.com>
2024-06-10 17:02:02 +0400

    adada

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,32 +1,31 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import * as z from 'zod'
-import { contactSchema } from '@/schemas/contact-schema'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import * as z from "zod";
+import { contactSchema } from "@/schemas/contact-schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
   FormField,
   FormControl,
   FormMessage,
   FormItem,
-  FormLabel
-} from '@/components/ui/form'
-import { useTransition } from 'react'
-import { Button } from './ui/button'
-import { toast } from 'sonner'
-import { sendContactEmail } from '@/server/send-contact-email'
+  FormLabel,
+} from "@/components/ui/form";
+import { useTransition } from "react";
+import { Button } from "./ui/button";
+import { toast } from "sonner";
+import { sendContactEmail } from "@/server/send-contact-email";
 
 const Contact = () => {
-
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const section = document.getElementById('contact');
+      const section = document.getElementById("contact");
       if (section) {
         const { top } = section.getBoundingClientRect();
         const isVisible = top >= 0 && top <= window.innerHeight;
@@ -34,79 +33,80 @@ const Contact = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  } , [])
-  const [isPending, startTransition] = useTransition()
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  const [isPending, startTransition] = useTransition();
 
   const form = useForm<z.infer<typeof contactSchema>>({
     resolver: zodResolver(contactSchema),
     defaultValues: {
-      name: '',
-      lastname: '',
-      email: '',
-      message: ''
-    }
-  })
+      name: "",
+      lastname: "",
+      email: "",
+      message: "",
+    },
+  });
 
   const handleContactSubmit = (values: z.infer<typeof contactSchema>) => {
     startTransition(() => {
       sendContactEmail(values).then((data) => {
         if (data.error) {
-          toast.error(data.error)
+          toast.error(data.error);
         }
         if (data.success) {
-          form.reset()
-          toast.success(data.success)
+          form.reset();
+          toast.success(data.success);
         }
-      })
-    })
-  }
-
+      });
+    });
+  };
 
   return (
-    <section id='contact' className='w-full py-6 md:py-12 lg:py-20 border-t'>
-      <div className='container grid items-center gap-4 px-4 md:px-4'>
-        <div className='space-y-4'>
-          <div className='inline-block rounded-lg bg-primary px-3 py-1 text-sm text-white'>
-            Projects
+    <section id="contact" className="w-full py-6 md:py-12 lg:py-20 border-t">
+      <div className="container grid items-center gap-4 px-4 md:px-4">
+        <div className="space-y-4">
+          <div className="inline-block rounded-lg bg-primary px-3 py-1 text-sm text-white">
+            Contact
           </div>
-          <div className='space-y-10'>
-          <motion.div initial="hidden" animate={isVisible ? "visible" : "hidden"} variants={{
-              hidden: {
-                scale: .8,
-                opacity: 0
-              },
-              visible: {
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: .2
-                }
-              },
-            }}>
-            <h2 className='text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-primary'>
-              Contact me
-            </h2>
+          <div className="space-y-10">
+            <motion.div
+              initial="hidden"
+              animate={isVisible ? "visible" : "hidden"}
+              variants={{
+                hidden: {
+                  scale: 0.8,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                  },
+                },
+              }}>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl bg-clip-text text-transparent bg-gradient-to-tr from-primary">
+                Contact me
+              </h2>
             </motion.div>
-            <div className='mx-auto w-full max-w-[700px]'>
+            <div className="mx-auto w-full max-w-[700px]">
               <Form {...form}>
                 <form
-                  className='grid gap-4'
-                  onSubmit={form.handleSubmit(handleContactSubmit)}
-                >
-                  <div className='grid sm:grid-cols-2 gap-4'>
+                  className="grid gap-4"
+                  onSubmit={form.handleSubmit(handleContactSubmit)}>
+                  <div className="grid sm:grid-cols-2 gap-4">
                     <FormField
                       control={form.control}
-                      name='name'
+                      name="name"
                       render={({ field }) => (
                         <FormItem>
                           <FormLabel>Name</FormLabel>
                           <FormControl>
                             <Input
                               {...field}
-                              placeholder='John'
-                              type='text'
+                              placeholder="John"
+                              type="text"
                               disabled={isPending}
                             />
                           </FormControl>
@@ -116,15 +116,15 @@ const Contact = () => {
                     />
                     <FormField
                       control={form.control}
-                      name='lastname'
+                      name="lastname"
                       render={({ field }) => (
                         <FormItem>
                           <FormLabel>Last Name</FormLabel>
                           <FormControl>
                             <Input
                               {...field}
-                              placeholder='Doe'
-                              type='text'
+                              placeholder="Doe"
+                              type="text"
                               disabled={isPending}
                             />
                           </FormControl>
@@ -135,15 +135,15 @@ const Contact = () => {
                   </div>
                   <FormField
                     control={form.control}
-                    name='email'
+                    name="email"
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel>E-mail</FormLabel>
                         <FormControl>
                           <Input
                             {...field}
-                            placeholder='info@example.com'
-                            type='email'
+                            placeholder="info@example.com"
+                            type="email"
                             disabled={isPending}
                           />
                         </FormControl>
@@ -153,14 +153,14 @@ const Contact = () => {
                   />
                   <FormField
                     control={form.control}
-                    name='message'
+                    name="message"
                     render={({ field }) => (
                       <FormItem>
                         <FormLabel>Message</FormLabel>
                         <FormControl>
                           <Textarea
                             {...field}
-                            placeholder='Your message'
+                            placeholder="Your message"
                             disabled={isPending}
                             rows={6}
                           />
@@ -170,10 +170,9 @@ const Contact = () => {
                     )}
                   />
                   <Button
-                    variant='secondary'
-                    type='submit'
-                    disabled={isPending}
-                  >
+                    variant="secondary"
+                    type="submit"
+                    disabled={isPending}>
                     Send
                   </Button>
                 </form>
@@ -183,7 +182,7 @@ const Contact = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
